ac75c1dbf4941a115c963aeedf9257b067e2ecbd
Santos yosmar rios torrez <riossantos340@gmail.com>
2025-03-10 23:00:00 -0600

    add:Documental

--- src/componentes/Documental.jsx
+++ src/componentes/Documental.jsx
@@ -3,7 +3,7 @@ function Documental() {
  
     return (
       <>
-       <h5>El documental es la expresión de un aspecto de la realidad, mostrada en
+       <h3>El documental es la expresión de un aspecto de la realidad, mostrada en
          forma audiovisual. La organización y estructura de imágenes y sonidos (textos
           y entrevistas), según el punto de vista del autor, determina el tipo de documental.
           En otras palabras, un documental es un medio audiovisual dónde se muestran hechos 
@@ -15,7 +15,7 @@ amplia en la actualidad, que van desde el documental puro hasta documentales de 
 pasando por modelos de reportajes muy variados, el docudrama (formato en el que los personajes 
 reales se interpretan a sí mismos), llegando hasta el falso documental.
 
-</h5>
+</h3>
       </>
     )
   }
